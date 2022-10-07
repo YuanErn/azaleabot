003f69c034b1cd58e814875af17a783ce65821d3
YuanErn <yuanernc@gmail.com>
2022-10-07 22:57:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import hikari
+import lightbulb
 
 tokenFile = open('TOKEN', 'r')
 token = tokenFile.readline()
 tokenFile.close()
-bot = hikari.GatewayBot(token)
+bot = lightbulb.BotApp(token)
 
 @bot.listen(hikari.GuildMessageCreateEvent)
 async def print_message(event):
     print(event.content)
 
-
+@bot.listen(hikari.StartedEvent)
+async def on_started(event):
+    print ('Bot has started')
+    
 bot.run()
