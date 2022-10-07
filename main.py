70767c5b02abbb6a798919dfc13b337c7c447d40
YuanErn <yuanernc@gmail.com>
2022-10-08 00:59:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import hikari
 import lightbulb
 import discord
-from discord.ext import commands
+import asyncio
 
 #tokenfile
 tokenFile = open('TOKEN', 'r')
@@ -23,15 +23,56 @@ bot = lightbulb.BotApp(
 async def print_message(event):
     print(event.content)
 
-@bot.command
-@lightbulb.command('socials', 'Displays the socials')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def embed(ctx):
-    embed = discord.Embed(title="Socials!", description="Here are the socials", color=discord.Colour.random())
-    embed.add_field(name="Instagram", value="Ig link here")
-    embed.add_field(name="Facebook", value="Fb link here")
-    embed.add_field(name="Twitter", value="Tw link here")
+PLATFORMS = {
+    "Instagram": "ig",
+    "Facebook": "🐱",
+    "Twitter": "🐼",
+    "Twitch": "🦊",
+}
 
-    await ctx.respond(embed = embed)
+@bot.command
+@lightbulb.command('group','This is a group')
+@lightbulb.implements(lightbulb.SlashCommandGroup)
+async def social_group(ctx):
+    pass
+
+@social_group.child
+@lightbulb.command("socials", "Get the link to the socials")
+@lightbulb.implements(lightbulb.PrefixSubCommand, lightbulb.SlashSubCommand)
+async def platform_subcommand(ctx: lightbulb.Context) -> None:
+    select_menu = (
+        ctx.bot.rest.build_action_row()
+        .add_select_menu("platform_select")
+        .set_placeholder("Pick a platform")
+    )
+
+    for name, emoji in PLATFORMS.items():
+        select_menu.add_option(
+            name,  # the label, which users see
+            name.lower().replace(" ", "_"),  # the value, which is used by us later
+        ).set_emoji(emoji).add_to_menu()
+
+    resp = await ctx.respond(
+        "Pick a platform from the dropdown :3",
+        component=select_menu.add_to_container(),
+    )
+    msg = await resp.message()
+
+    try:
+        event = await ctx.bot.wait_for(
+            hikari.InteractionCreateEvent,
+            timeout=60,
+            predicate=lambda e: isinstance(e.interaction, hikari.ComponentInteraction)
+            and e.interaction.user.id == ctx.author.id
+            and e.interaction.message.id == msg.id
+            and e.interaction.component_type == hikari.ComponentType.SELECT_MENU,
+        )
+    except asyncio.TimeoutError:
+        await msg.edit("The menu timed out :c", components=[])
+    else:
+        platform = event.interaction.values[0]
+        await msg.edit(
+            f"Here's the link to our {platform} page for you! :3", embed=embed, components=[]
+        )
 
 bot.run()
