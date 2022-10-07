8cd350013951a9577bdd38bf5ffbfaa0af0ed5ff
YuanErn <106149361+YuanErn@users.noreply.github.com>
2022-10-07 23:34:09 +0800

    Delete social ping.py

--- social ping.py
+++
@@ -1,8 +0,0 @@
-import lightbulb
-
-@bot.command
-@lightbulb.command('socials', 'Replies with pong')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def ping(ctx):
-    await ctx.respond('Pong!')
-
