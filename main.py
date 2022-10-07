0ea170003ffbe5f764042d2a99baaa21a19b6dd4
YuanErn <yuanernc@gmail.com>
2022-10-07 23:50:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ tokenFile.close()
 # guildFile.close()
 
 #initialisation
-bot = lightbulb.BotApp(token, default_enabled_guilds = (999584580019441728))
+bot = lightbulb.BotApp(
+    token, 
+    default_enabled_guilds = (999584580019441728)
+    )
 
 @bot.listen(hikari.GuildMessageCreateEvent)
 async def print_message(event):
