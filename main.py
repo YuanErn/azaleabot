60ef0dd0a725f78f8c82cd8be7229a696d9c1183
YuanErn <yuanernc@gmail.com>
2022-10-07 23:25:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def print_message(event):
     print(event.content)
 
 @bot.listen(hikari.StartedEvent)
-async def on_started():
+async def on_started(event):
     print ('Bot has started!')
 
 @bot.command
