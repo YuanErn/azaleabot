bd09f4aa088d2018e2535101df5d6ebdebd5edd0
YuanErn <yuanernc@gmail.com>
2022-10-07 22:41:51 +0800

    Added start message

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import hikari
 
-#hiding token
 tokenFile = open('TOKEN', 'r')
 token = tokenFile.readline()
 tokenFile.close()
-
 bot = hikari.GatewayBot(token)
+
+@bot.listen(hikari.StartedEvent)
+async def bot_started(event):
+    print("Bot is online")
+
 bot.run()
