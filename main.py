c4bbccdf00648846336c79b1957b59e2f0952598
YuanErn <yuanernc@gmail.com>
2022-10-07 23:25:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ bot = lightbulb.BotApp(token, default_enabled_guilds = (999584580019441728))
 async def print_message(event):
     print(event.content)
 
-@bot.event
-async def on_ready():
-   await client.get_channel("enter channel id here").send("bot is online")
+@bot.listen(hikari.StartedEvent)
+async def on_started():
+    print ('Bot has started!')
 
 @bot.command
 @lightbulb.command('ping', 'Replies with pong')
