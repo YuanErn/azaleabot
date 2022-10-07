712b3e2f5ba61d088cbed19718ebaeb68e99340c
YuanErn <yuanernc@gmail.com>
2022-10-07 23:05:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import hikari
 tokenFile = open('TOKEN', 'r')
 token = tokenFile.readline()
 tokenFile.close()
-bot = lightbulb.BotApp(token)
+
+bot = lightbulb.BotApp(token, default_enabled_guilds = (999584580019441728))
 
 @bot.listen(hikari.GuildMessageCreateEvent)
 async def print_message(event):
@@ -13,5 +14,12 @@ async def print_message(event):
 @bot.listen(hikari.StartedEvent)
 async def on_started(event):
     print ('Bot has started')
+
+@bot.command
+@lightbulb.command('Ping', 'Replies with pong')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('Pong!')
+
 
 bot.run()
