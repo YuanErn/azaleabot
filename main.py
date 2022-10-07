e8b6ebf550d5a719803670589bb54bfb6bd1a02a
YuanErn <yuanernc@gmail.com>
2022-10-07 23:55:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ bot = lightbulb.BotApp(
 async def print_message(event):
     print(event.content)
 
-# @bot.command
-# @lightbulb.command('socials', 'Displays the socials')
-# @lightbulb.implements(lightbulb.SlashCommand)
-# async def ping(ctx):
-#     await ctx.respond(embed = discord.Embed(title="Socials", description="These are the socials", color= "Blue", )
+@bot.command
+@lightbulb.command('socials', 'Displays the socials')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond(embed = discord.Embed(title="Socials", description="These are the socials", color= "Blue")
 
 bot.run()
