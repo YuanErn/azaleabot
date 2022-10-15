2be2ca1fe0b623de75fcf9e926acd72d9b44bd60
YuanErn <yuanernc@gmail.com>
2022-10-16 01:10:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,17 +34,17 @@ async def ping(ctx):
     embed.add_field(name="Facebook", value="[Here is the link!](https://www.facebook.com/profile.php?id=100086242737895)")
     await ctx.respond(embed=embed)
 
-@bot.command
-@lightbulb.command('signup', 'Sends the signup form straight to your dms')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def signup(ctx: lightbulb.Context):
-    embed = hikari.Embed(title="Here is the signup!", description="empty for now", color=0x9b59b6)
-    embed.add_field(name="Tiktok", value="[Here is the link!](https://www.tiktok.com/@adrenaline_esports)")
-    embed.add_field(name="Instagram", value="[Here is the link!](https://www.instagram.com/adll.esports)")
-    embed.add_field(name="Twitter", value="[Here is the link!](https://twitter.com/ADL_Esports)")
-    embed.add_field(name="Twitch", value="[Here is the link!](https://www.twitch.tv/adrenaline_esports_apac)")
-    embed.add_field(name="Facebook", value="[Here is the link!](https://www.facebook.com/profile.php?id=100086242737895)")
-    await ctx.respond(f"The necessary details have been sent into your DMs, good luck!" embed=embed)
+# @bot.command
+# @lightbulb.command('signup', 'Sends the signup form straight to your dms')
+# @lightbulb.implements(lightbulb.SlashCommand)
+# async def signup(ctx: lightbulb.Context):
+#     embed = hikari.Embed(title="Here is the signup!", description="empty for now", color=0x9b59b6)
+#     embed.add_field(name="Tiktok", value="[Here is the link!](https://www.tiktok.com/@adrenaline_esports)")
+#     embed.add_field(name="Instagram", value="[Here is the link!](https://www.instagram.com/adll.esports)")
+#     embed.add_field(name="Twitter", value="[Here is the link!](https://twitter.com/ADL_Esports)")
+#     embed.add_field(name="Twitch", value="[Here is the link!](https://www.twitch.tv/adrenaline_esports_apac)")
+#     embed.add_field(name="Facebook", value="[Here is the link!](https://www.facebook.com/profile.php?id=100086242737895)")
+#     await ctx.respond(f"The necessary details have been sent into your DMs, good luck!" embed=embed)
     
 
 bot.run()
