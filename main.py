2f5328538671afe248251cb1f7829fe6719e7575
YuanErn <yuanernc@gmail.com>
2022-12-02 14:06:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,22 +19,22 @@ bot = lightbulb.BotApp(
     guilds
     )
 
-@bot.listen(hikari.MessageCreateEvent)
+@bot.listen(hikari.GuildMessageCreateEvent)
 async def print_message(event):
     print(event.content)
 
 #/socials
-@bot.command
-@lightbulb.command('socials', 'Displays the socials')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def ping(ctx):
-    embed = hikari.Embed(title="Socials!", description="Here are the socials :)", color=0x9b59b6)
-    embed.add_field(name="Tiktok", value="[Here is the link!](https://www.tiktok.com/@adrenaline_esports)")
-    embed.add_field(name="Instagram", value="[Here is the link!](https://www.instagram.com/adll.esports)")
-    embed.add_field(name="Twitter", value="[Here is the link!](https://twitter.com/ADL_Esports)")
-    embed.add_field(name="Twitch", value="[Here is the link!](https://www.twitch.tv/adrenaline_esports_apac)")
-    embed.add_field(name="Facebook", value="[Here is the link!](https://www.facebook.com/profile.php?id=100086242737895)")
-    await ctx.respond(embed=embed)
+# @bot.command
+# @lightbulb.command('socials', 'Displays the socials')
+# @lightbulb.implements(lightbulb.SlashCommand)
+# async def ping(ctx):
+#     embed = hikari.Embed(title="Socials!", description="Here are the socials :)", color=0x9b59b6)
+#     embed.add_field(name="Tiktok", value="[Here is the link!](https://www.tiktok.com/@adrenaline_esports)")
+#     embed.add_field(name="Instagram", value="[Here is the link!](https://www.instagram.com/adll.esports)")
+#     embed.add_field(name="Twitter", value="[Here is the link!](https://twitter.com/ADL_Esports)")
+#     embed.add_field(name="Twitch", value="[Here is the link!](https://www.twitch.tv/adrenaline_esports_apac)")
+#     embed.add_field(name="Facebook", value="[Here is the link!](https://www.facebook.com/profile.php?id=100086242737895)")
+#     await ctx.respond(embed=embed)
 
 #logs the server's chats
 # @bot.listen(hikari.GuildMessageCreateEvent)
