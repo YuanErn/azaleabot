effede65b0a85318ed1f76c92d828d0c1ba264a6
YuanErn <yuanernc@gmail.com>
2022-12-02 13:59:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ tokenFile = open('TOKEN', 'r')
 token = tokenFile.readline()
 tokenFile.close()
 
-#initialisation
-bot = lightbulb.BotApp(token)
+#guildfile
+guildFile = open('GUILDS', 'r')
+guilds  = guildFile.readline()
+guildFile.close()
 
-@bot.listen(hikari.MessageCreateEvent)
+#initialisation
+bot = lightbulb.BotApp(
+    token,
+    guilds
+    )
+
+@bot.listen(hikari.GuildMessageCreateEvent)
 async def print_message(event):
-    print(event.content , event.author)
+    print(event.content)
 
 #/socials
 @bot.command
