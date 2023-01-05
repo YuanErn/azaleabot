2c42efeeb6e1d2c280ea197fcf36e2490f118cb4
YuanErn <yuanernc@gmail.com>
2023-01-06 02:48:57 +0800

    Update autoClaim.py

--- autoClaim.py
+++ autoClaim.py
@@ -20,12 +20,10 @@ async def autoClaim():
         print(f"Claimed {reward.amount}x {reward.name}")
 
 
-autoClaim()
-
 @daily_plugin.listener(hikari.StartedEvent)
 async def on_started(_: hikari.StartedEvent) -> None:
     # This event fires once, when the BotApp is fully started.
-    daily_plugin.app.d.sched.add_job(autoClaim, CronTrigger(minute="*/1"))
+    daily_plugin.app.d.sched.add_job(autoClaim, CronTrigger(day="*/1"))
 
 def load(bot: lightbulb.BotApp) -> None:
     bot.add_plugin(daily_plugin)
