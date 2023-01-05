3f5e7bcab69eba039aa77a63298c3c6c383c7a1c
YuanErn <yuanernc@gmail.com>
2023-01-06 02:31:36 +0800

    Update autoClaim.py

--- autoClaim.py
+++ autoClaim.py
@@ -7,20 +7,20 @@ from apscheduler.triggers.cron import CronTrigger
 daily_plugin = lightbulb.Plugin("Daily")
 onlineList = []
 
-async def autoClaim():
+def autoClaim():
     cookies = {"ltuid": 25089082, "ltoken": "4P3cU29tyXw2pVSS01MukdJhvSPF16GoGi7gQJxF"}
     client = genshin.Client(cookies, game=genshin.Game.GENSHIN)
 
     # claim daily reward
     try:
-        reward = await client.claim_daily_reward()
+        reward = client.claim_daily_reward()
     except genshin.AlreadyClaimed:
         print("Daily reward already claimed")
     else:
         print(f"Claimed {reward.amount}x {reward.name}")
 
 
-asyncio.run(autoClaim())
+autoClaim()
 
 @daily_plugin.listener(hikari.StartedEvent)
 async def on_started(_: hikari.StartedEvent) -> None:
